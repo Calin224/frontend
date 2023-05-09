b175539057d50d769e5a45373a6b760d5747d027
Calin224 <sanducalinm@gmail.com>
2023-05-09 08:22:56 -0700

    before googleauth strapi

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,10 +9,6 @@ export const authOptions = {
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
-        DiscordProvider({
-            clientId: process.env.DISCORD_CLIENT_ID,
-            clientSecret: process.env.DISCORD_CLIENT_SECRET,
-        }),
     ],
     callbacks: {
         async jwt({ token, account }) {
